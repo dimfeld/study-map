ac76b15dab86ea62e913e772127df38fe410ba17
Daniel Imfeld <daniel@imfeld.dev>
2021-01-04 19:41:16 +0000

    Fix duration

--- src/FadeRoute.svelte
+++ src/FadeRoute.svelte
@@ -2,11 +2,11 @@
   import { fade } from 'svelte/transition';
   import { Route } from 'tinro';
 
-  export let delay = 200;
+  export let duration = 100;
 </script>
 
 <Route {...$$restProps} let:meta>
-  <div transition:fade={{ delay }}>
+  <div transition:fade|local={{ duration }}>
     <slot {meta} />
   </div>
 </Route>
